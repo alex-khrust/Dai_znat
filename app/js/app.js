b76b28db0291ddc08088c736c7528e9e7d4b9bbc
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-09-15 18:04:04 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+// import $ from 'jquery'; window.jQuery = $; window.$ = $;
+// require('~/app/libs/imageuploadify/imageuploadify.min.js');
+// import imageuploadify from '~/app/libs/imageuploadify/imageuploadify.min.js'; window.imageuploadify = imageuploadify;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+	$('.input-images').imageUploader();
+	
+
+})
