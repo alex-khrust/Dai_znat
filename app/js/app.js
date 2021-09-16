214540cafa6d4f4795626ec46da01c8ddc33f17c
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-09-16 10:15:18 +0300

    Добавление Raleway medium

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,8 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 // import $ from 'jquery'; window.jQuery = $; window.$ = $;
-// require('~/app/libs/imageuploadify/imageuploadify.min.js');
-// import imageuploadify from '~/app/libs/imageuploadify/imageuploadify.min.js'; window.imageuploadify = imageuploadify;
+// require('~/app/libs/imageuploadify/imageuploadify.m;in.js');
+// import imageuploadify from 'libs/imageuploadify/imageuploadify.min.js'; window.imageuploadify = imageuploadify
 
 document.addEventListener('DOMContentLoaded', () => {
 
