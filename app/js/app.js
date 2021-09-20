f22b1548b2f7ca8dc443fe57f5bc0206e390f2ff
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-09-20 17:31:10 +0300

    Добавлены страницы enter и authorization

--- app/js/app.js
+++ app/js/app.js
@@ -5,10 +5,45 @@
 // require('~/app/libs/imageuploadify/imageuploadify.m;in.js');
 // import imageuploadify from 'libs/imageuploadify/imageuploadify.min.js'; window.imageuploadify = imageuploadify
 
+// import Vue from 'vue'
+// import vSelect from 'vue-select'
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 	$('.input-images').imageUploader();
 	
 
-})
+	//------------ menu hamburger ----------------------------------
+  $("#navToggle").click(function () {
+    $(this).toggleClass("active");
+    $(".header__authorization, .menu").toggleClass("open");
+    $("body").toggleClass("locked");
+  });
+  $(".top-nav").click(function () {
+    $(this).removeClass("active");
+    $("#navToggle").removeClass("active");
+    $(".header__authorization, .menu").removeClass("open");
+    $("body").removeClass("locked");
+  });
+  //---------------------------------------------------------------------------
+
+  //---------------------------------------------------------------------------
+  
+});
+
+
+// VUE ----------------------------------------------------------------------
+// Vue.component('v-select', VueSelect.VueSelect)
+
+// new Vue({
+//   el: '#local',
+//   // data: {
+//   //   options: [
+//   //     'foo',
+//   //     'bar',
+//   //     'baz'
+//   //   ]
+//   // }
+// });
