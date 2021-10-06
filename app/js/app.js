30dfe27075b5c24e6d70af83648a851dbaf628c7
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-10-06 21:29:51 +0300

    Добавление страницы Заведения

--- app/js/app.js
+++ app/js/app.js
@@ -68,7 +68,66 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
+
+
+
   
+  // SELECT ----------------------------------------------------------------
+  $('select').each(function(index){
+  
+    var sel = $(this);
+    
+    // MAKE CUSTOM SELECT ELEMENTS
+    // index here used to identify select element if more
+    // than one in the page
+    sel.wrap('<div class="wrap-select wrap-select-' + index + '"></div>');
+    var $wrap_sel= $('.wrap-select-' + index);
+    
+    $wrap_sel.append('<div class="custom-select">YAY</div>');
+    $wrap_sel.append('<ul class="custom-select-items"></ul>');
+    var $custom_select = $wrap_sel.find('.custom-select'),
+        $sel_items = $wrap_sel.find('.custom-select-items');
+    
+    //Popolate the custom options
+    sel.children('option').each(function(index){
+     //Separation between placeholder and select items
+     if($(this).val() === 'default'){
+      $custom_select.text($(this).text());
+     }
+     else{
+      $sel_items.append('<li>' + $(this).text() +'</li>');
+     }
+    });
+    
+    
+    //DISPLAY CUSTOM OPTIONS HANDLER
+    $custom_select.on('click', function(){
+     $(this).toggleClass('active');
+     $sel_items.slideToggle();
+    });
+    
+    
+    //ITEM SELECTION
+    $sel_items.children('li').on('click', function(){
+      var itemVal = $(this).text();
+      $custom_select.text(itemVal).removeClass('active');
+      $sel_items.children('li').removeClass('active');
+      $(this).addClass('active');
+      $sel_items.slideUp();
+   
+      //set the original select option item for post data
+      sel.children('option').each(function(index){
+       if($(this).text() === itemVal){
+         $(this).attr('selected', 'selected');
+       }
+      });
+    });
+    
+   // End select each loop 
+   });
+
+  
+   // =======================================================================
 });
 
 
